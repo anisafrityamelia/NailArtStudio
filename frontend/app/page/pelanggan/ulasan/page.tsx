60f4c595d5cabc5da847f0b93b3b28f09398399b
Anisa Frity Amelia <anisafrityamelia@gmail.com>
2026-04-16 14:43:07 +0700

    membuat layout pelanggan

---
+++ frontend/app/page/pelanggan/ulasan/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import Judul_Halaman from "@/app/components/ui/judul_halaman";
+
+export default function UlasanPage() {
+    return (
+        <>
+            <section>
+                {/* Judul halaman */}
+                <Judul_Halaman title="Kelola Ulasan" />
+            </section>
+        </>
+    );
+}
